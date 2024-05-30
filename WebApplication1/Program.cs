e9ab0d66e3f4ef62aa4638ca35d0a780247ff414
render41 <gabrielservice05@gmail.com>
2024-05-30 11:20:44 -0300

    Learn Program class

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,11 @@
+// Configure Builder
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure App
 var app = builder.Build();
 
+// Behaviors App
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/testes", () => "Test App");
 
 app.Run();
